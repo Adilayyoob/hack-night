642b130b0044b14630dd576089fba7d83c218ab5
unknown <adilkuyyo@gmail.com>
2021-09-04 22:56:04 +0530

    new

---
+++ script.js
@@ -0,0 +1,34 @@
+function start() {
+    var username = document.form1.username.value;
+    var url = "https://api.github.com/users/"+username;
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open("GET",url);
+	ourRequest.onload = function(){
+		var ourData = JSON.parse(ourRequest.responseText);
+		enter(ourData);
+	}
+	ourRequest.send();
+}
+
+function enter(d) {
+    document.getElementById("avatar").setAttribute("src",d.avatar_url)
+    document.getElementById("name").innerHTML = "Name : " + d.name;
+    document.getElementById("description").innerHTML = "description : " + d.bio;
+    document.getElementById("username").innerHTML = "Username : " + d.login;
+    document.getElementById("gitlink").innerHTML = "GitHub Link : " + d.html_url;
+    document.getElementById("followers").innerHTML = "Followers : " + d.followers;
+    document.getElementById("following").innerHTML = "Following : " + d.following;
+    document.getElementById("email").innerHTML = "Email ID : " + d.email;
+    document.getElementById("twitter").innerHTML = "Twitter : " + d.twitter_username;
+    document.getElementById("address").innerHTML = "Location : " + d.location;
+
+    var container = document.getElementById("Followers&Following");
+    for( var i=1;i <=9; i++)
+    {
+      var el = document.createElement("div");
+      el.className ="card";
+      el.id = "card" + i;
+      el.innerHTML = "ok";
+      container.append(el);
+    }
+}
